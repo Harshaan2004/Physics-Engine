8cca5faa0e5cc7d98295d6e06a1a80c4ac1e5993
Harshaan <harshaan124@gmail.com>
2025-08-12 17:36:55 +0100

    Update Space_Engine.cpp

--- Space_Engine.cpp
+++ Space_Engine.cpp
@@ -8,10 +8,7 @@
 //  SpaceEngine is a simple OpenGL-based simulation engine for educational purposes.
 //  It demonstrates basic physics and rendering of space using OpenGL and GLFW.
 //
-//  SpaceEngine is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+//  You can redistribute it and/or modify however you like!!!
 //
 //  Updated for 3D rendering with camera controls and shaders
 //
